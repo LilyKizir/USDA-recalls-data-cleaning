e65f6ab66ae996294527a50e8a16d5199cef4225
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2026-09-16 18:53:26 +0000

    installing python notebooks

--- data-cleaning/usda_data.py
+++ data-cleaning/usda_data.py
@@ -1,10 +1,7 @@
-import requests
+import pandas as pd
 
-url = "https://www.fsis.usda.gov/fsis/api/recall/v/1?field_states_id=All&field_archive_recall=All&field_closed_date_value=&field_closed_year_id=All&field_risk_level_id=All&field_processing_id=All&field_product_items_value=meat&field_recall_classification_id=All&field_recall_number=&field_recall_reason_id=All&field_recall_type_id=All&field_related_to_outbreak=All&field_summary_value=&field_year_id=All&field_translation_language=All"
+file_path = 'data-cleaning/usda_recalls.json'
 
-headers = {
-    "User-Agent": "data analytics personal project @ thedataschool (contact:)"
-}
+df = pd.read_json(file_path)
 
-response = requests.get(url,headers=headers)
-print(response)
+print(df)
